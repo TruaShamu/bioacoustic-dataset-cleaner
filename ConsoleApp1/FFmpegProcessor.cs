adffe19c5835facd1d6c5f86da919ce5e29d6159
unknown <sofia_mit@hotmail.com>
2023-10-14 16:21:14 +0000

    Add .gitignore to untrack certain files

---
+++ ConsoleApp1/FFmpegProcessor.cs
@@ -0,0 +1,43 @@
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Diagnostics;
+
+
+// Run ffmpeg command. Need to update ffmpeg to local path.
+public class FFmpegProcessor {
+    public static string ffmpegPath = @"C:\ProgramData\chocolatey\bin\ffmpeg.exe";
+    public static void FFmpegExecRunner(string ffmpegArgs) {
+        
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = ffmpegPath,
+            Arguments = ffmpegArgs,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardInput = true // Redirect the standard input
+        };
+
+        using (Process process = new Process { StartInfo = startInfo })
+        {
+            process.OutputDataReceived += (s, e) => Console.WriteLine(e.Data); // Handle standard output
+            process.ErrorDataReceived += (s, e) => Console.WriteLine(e.Data);  // Handle standard error
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+
+            if (process.ExitCode == 0)
+            {
+                Console.WriteLine("FFmpeg command executed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("FFmpeg command execution failed.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Diagnostics;
+
+public class DataCleaning {
+
+    // Azure Speech Resource Secrets
+    public static string speechServiceKey = "b54b9731878a4f4083c6b41dc774a92c";
+    public static string speechRegion = "eastus";
+
+    // Return a list of speech timestamp (start, end) in seconds
+    public static async Task<List<(double, double)>> ListSpeechTimeStamp(string inputFilePath) {
+        var config = SpeechConfig.FromSubscription(speechServiceKey, speechRegion);
+
+        // Language auto-detection (English, Spanish, Portuguese)        
+        var autoDetectSourceLanguageConfig = AutoDetectSourceLanguageConfig.FromLanguages(new string[] { "en-US", "es-ES", "pt-BR" });
+        var stopRecognition = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        // Speech timestamps
+        List<(double startTicks, double endTicks)> speechTimestamps = new List<(double, double)>();
+
+        using (var audioInput = AudioConfig.FromWavFileInput(inputFilePath))
+        {
+            using (var recognizer = new SpeechRecognizer(config, autoDetectSourceLanguageConfig, audioInput))
+            {
+                recognizer.Recognized += (s, e) =>
+                {
+                    // Speech recognized
+                    if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                    {
+                        var autoDetectSourceLanguageResult = AutoDetectSourceLanguageResult.FromResult(e.Result);
+                        long startTicks = e.Result.OffsetInTicks;
+                        long endTicks = startTicks + e.Result.Duration.Ticks;
+                        string recognizedText = e.Result.Text;
+                        speechTimestamps.Add((startTicks / 10000000, endTicks / 10000000));
+                        Console.WriteLine($"RECOGNIZED: Text={recognizedText}");
+                    }
+                };
+
+                recognizer.Canceled += (s, e) =>
+                {
+                    Console.WriteLine($"CANCELED: Reason={e.Reason}");
+                    if (e.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                    stopRecognition.TrySetResult(0);
+                };
+
+                recognizer.SessionStarted += (s, e) =>
+                {
+                    Console.WriteLine("\n    Session started event.");
+                };
+
+                recognizer.SessionStopped += (s, e) =>
+                {
+                    Console.WriteLine("\n    Session stopped event.");
+                    Console.WriteLine("\nStop recognition.");
+                    stopRecognition.TrySetResult(0);
+                };
+
+                await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+                Task.WaitAny(new[] { stopRecognition.Task });
+                await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+            }
+        }
+        return speechTimestamps;
+    }
+
+    // Build the ffmpeg command for removing all the timestamps from the audio!
+    public static string buildRemovalFFmpegCommand(string inputFilePath, string outputFilePath, List<(double startTicks, double endTicks)> speechTimestamps) {
+        string range = "";
+        for (int i=0; i < speechTimestamps.Count; i++) {
+            range+="between(t," + speechTimestamps[i].startTicks + "," + speechTimestamps[i].endTicks + ")";
+            if (i != speechTimestamps.Count -1) {
+                range+="+";
+            }
+        }
+        return  $"-i {inputFilePath} -af \"aselect='not({range})'\" {outputFilePath}";
+    }
+
+    // FFmpeg Build command for conversion mp3 to wav
+}
